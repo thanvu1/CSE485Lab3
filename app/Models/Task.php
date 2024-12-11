fe9850f2352e3705da3ca2268f1b821429c9088a
thanvu1 <hoangthanvu11@gmail.com>
2024-12-11 15:06:08 +0700

    .

---
+++ app/Models/Task.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    // /**
+    //  * Các cột có thể được gán hàng loạt.
+    //  *
+    //  * @var array
+    //  */
+    // protected $fillable = [
+    //     'title',
+    //     'description',
+    //     'long_description',
+    //     'completed',
+    // ];
+
+    // /**
+    //  * Cấu hình kiểu dữ liệu tự động cast.
+    //  *
+    //  * @var array
+    //  */
+    // protected $casts = [
+    //     'completed' => 'boolean',
+    // ];
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
+// Route định nghĩa
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tasks', TaskController::class);
